4808d5be21e9aed8e65ca4865e8a9dd8f2193c13
Pavel Tikhachev <p.tihachev@uiscom.ru>
2019-02-28 17:07:32 +0300

    fix

--- lina_community_version/core/middleware.py
+++ lina_community_version/core/middleware.py
@@ -4,6 +4,6 @@ from aiohttp.web import middleware, Response
 @middleware
 async def check_group_middleware(request, handler):
     data = await request.json()
-    if data['group_id'] != request.dict_config.owner.cfg['group_id']:
+    if data['group_id'] != request.config_dict.owner.cfg['group_id']:
         return Response(status=400)  # Bad Request for invalid group id
     return await handler(request)
